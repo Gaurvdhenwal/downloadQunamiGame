e216e6d141b4635024a069b7eea3a7b7963d6860
gaurav <gauravdhenwal699@gmail.com>
2018-08-20 16:05:38 +0530

    UI and js optimization
    side padding removed
    fadingOut and fadeIn of divs 
    page title

--- download.js
+++ download.js
@@ -1,34 +1,35 @@
 $(function(){
   var url = decodeURIComponent(window.location.search.substring(1));
   var ghostMatchLink = getUrlParameter("ghost_match",url);
-  if(ghostMatchLink == undefined){
+  if(ghostMatchLink == undefined){//if not a ghost match link show normal animation
 
     $('#general-heading').css("display","block");
     $('#ghostMatch-heading').css("display","none");
     $('#ghostMatch-heading-2').css("display","none");
 
     //hiding and showing
-    $('.download_container').hide(function(){
-      $('.great_going').addClass('bounceIn');
-      setTimeout(function(){
-        console.log('inside function');
-        $('.great-going-cont').fadeOut('fast');
-        // $('great_going').addClass('zoomOut');
-      },1500);
-
-    });
+    $('.download_container').css("display",'none');
+    $('.great_going').addClass('bounceIn');
+    setTimeout(function(){
+      console.log('inside function');
+      $('.great-going-cont').fadeOut('fast');
+      // $('great_going').addClass('zoomOut');
+    },1500);
     setTimeout(function(){
       console.log('function two');
+        // $('.download_container').css("display",'block');
       $('.download_container').show('fast');
     },2000);
   }
   else{
+    $('.great-going-cont').css('display','none');
     $('.download_container').show('fast');
+
     console.log("link: "+ghostMatchLink);
     $('#general-heading').css("display","none");
     $('#ghostMatch-heading').css("display","block");
     $('#ghostMatch-heading-2').css("display","block");
-    $('.great-going-cont').css('display','none');
+
     var creator = getLinkValues("g_owner",ghostMatchLink);
     $('#ghost-name').html(creator);
   }
