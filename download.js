30d74cebda7c38b91ccb50b30b696e315334e1ce
gaurav <gauravdhenwal699@gmail.com>
2018-08-20 15:19:12 +0530

    ghost match link
    +download page name of creator
    +clipboard copy of ghost_match

--- download.js
+++ download.js
@@ -1,6 +1,13 @@
 $(function(){
-  var ghostMatchLink = getUrlParameter("ghostMatch");
+  var url = decodeURIComponent(window.location.search.substring(1));
+  var ghostMatchLink = getUrlParameter("ghost_match",url);
   if(ghostMatchLink == undefined){
+
+    $('#general-heading').css("display","block");
+    $('#ghostMatch-heading').css("display","none");
+    $('#ghostMatch-heading-2').css("display","none");
+
+    //hiding and showing
     $('.download_container').hide(function(){
       $('.great_going').addClass('bounceIn');
       setTimeout(function(){
@@ -16,12 +23,18 @@ $(function(){
     },2000);
   }
   else{
+    $('.download_container').show('fast');
     console.log("link: "+ghostMatchLink);
-    
+    $('#general-heading').css("display","none");
+    $('#ghostMatch-heading').css("display","block");
+    $('#ghostMatch-heading-2').css("display","block");
+    $('.great-going-cont').css('display','none');
+    var creator = getLinkValues("g_owner",ghostMatchLink);
+    $('#ghost-name').html(creator);
   }
 
-  function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+  function getUrlParameter(sParam,url) {
+    var sPageURL = url,
         sURLVariables = sPageURL.split('&'),
         sParameterName,
         i;
@@ -33,6 +46,14 @@ $(function(){
             return sParameterName[1] === undefined ? true : sParameterName[1];
         }
     }
-};
+}
+  function getLinkValues(param,link){
+
+    var linkObj = JSON.parse(link.split('?')[1]);
+    var name = linkObj[param].split('+')[0];
+    console.log(linkObj[param]);
+
+    return name;
+  }
 
 });
